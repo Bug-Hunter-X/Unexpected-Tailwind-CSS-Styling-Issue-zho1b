a61e2c74a8f276a7dbaad628fc74505168e5dfbf
BugHunter-X <askzenai@gmail.com>
2025-01-20 20:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+//some tailwind css code
+<div className="bg-gray-100 p-4">
+  <h2 className="text-2xl font-bold mb-2">Uncommon TailwindCSS Bug</h2>
+  <p className="text-gray-600">This is some text with an issue.</p>
+</div>
+```
